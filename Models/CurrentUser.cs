acf72b7d15243988601371126eeb7622d4496c90
Johan <krangh@gmail.com>
2024-01-15 16:11:02 +0100

    Added ViewModel and CurrentUser-class

---
+++ Models/CurrentUser.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP.NET_StudentRegisrty.Models
+{
+    public class CurrentUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = "";
+    }
+}
---
+++ ViewModels/StudentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ASP.NET_StudentRegisrty.Models;
+
+namespace ASP.NET_StudentRegisrty.ViewModels
+{
+    public class StudentViewModel
+    {
+        public Student Student { get; set; }
+        public CurrentUser CurrentUser { get; set; }
+    }
+}
